4512e35d75dafc183d0ce636180571ae321583cc
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-10-29 23:23:29 +0700

    mapStateToProps dan mapDispatchToProps

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,21 +1,38 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { addTodos } from '../redux/Reducer'
 
+const mapStateToProps = (state) => {
+  return{
+    todos : state,
+  }
+}
 
-const Todos = () => {
+const mapDispatchToProps = (dispatch) => {
+  return{
+    addTodo : (obj) => dispatch(addTodos(obj))
+  }
+}
+const Todos = (props) => {
   const [todo, setTodo] = useState("")
 
   const handleChange = (event) => {
     setTodo(event.target.value)
   }
-    
-  console.log(todo)
+      console.log("props from store", props)
+
   return (
     <div className='addTodos'>
         <input type="text" className='todo-input' onChange={ (event) => handleChange(event)}></input>
-        <button className='add-btn'>Add</button>
+        <button className='add-btn' onClick={() => props.addTodo({
+          id: Math.floor(Math.random()*1000),
+          item : todo,
+          completed : false
+
+        })}>Add</button>
 
     </div>
   )
 }
 
-export default Todos
+export default connect(mapStateToProps, mapDispatchToProps)(Todos)
