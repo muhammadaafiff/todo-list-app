f982ec315655f228e3a4d2a61debf8e994fc9f4a
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-10-29 22:52:20 +0700

    fix reducer

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -1,15 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = []
+const initialState = [];
 
 const addTodoReducer = createSlice({
-    name: "todos",
-    initialState,
-    reducers: {
-        //menambahkan todos
-        addTodos : (state, action) {
-            state.push(action.payload)
-            return state
-    }
-})
+  name: "todos",
+  initialState,
+  reducers: {
+    //menambahkan todos
+    addTodos: (state, action) => {
+      state.push(action.payload);
+      return state;
+    },
+  },
+});
 
+export const {addTodos} = addTodoReducer.actions;
+export const reducer = addTodoReducer.reducer
