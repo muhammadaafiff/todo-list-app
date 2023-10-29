da883592e2a77d40664efa5f2669f08c065311cd
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-10-29 22:41:07 +0700

    handle change todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Todos = () => {
+  const [todo, setTodo] = useState("")
+
+  const handleChange = (event) => {
+    setTodo(event.target.value)
+  }
     
+  console.log(todo)
   return (
     <div className='addTodos'>
-        <input type="text" className='todo-input'></input>
+        <input type="text" className='todo-input' onChange={ (event) => handleChange(event)}></input>
         <button className='add-btn'>Add</button>
 
     </div>
